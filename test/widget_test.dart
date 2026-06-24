5aeb7b1568ec5af0fd934db68b4565bafa1d163a
teacher019 <abdulkhalekt10@gmail.com>
2026-06-24 22:53:06 +0600

    intiale commit

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,12 +9,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:live_score_project_app/app.dart';
 
-import 'package:live_score_project_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const LiveScoreProjectApp());
+    await tester.pumpWidget(const LiveScoreApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
